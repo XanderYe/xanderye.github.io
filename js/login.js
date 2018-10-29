4455a5cc4d14467200c0472e5720388b8935dbb1
叶振东 <XanderYe@outlook.com>
2018-10-29 11:41:50 +0800

    Update login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
 $(function () {
-    $(".mc-login").click(function () {
-        this.style({display: "block", top: "27.203px", height: "217.594px"});
+    $(".mc-login-btn").click(function () {
+        $("body").addClass("mdui-locked");
+        $("body").css("width","1023.33px");
+        $(".mc-login").addClass("mdui-dialog-open");
+        $(".mc-login").css({display: "block", top: "27.203px", height: "217.594px"});
     });
 })
